3f3d763e0195326facc82eae933445e8aae24565
Aurora Scharff <aurora.sofie@gmail.com>
2026-04-06 16:37:21 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function HomePage({ searchParams }: PageProps<'/'>) {
                   </ViewTransition>
                 }
               >
-                <ViewTransition enter="slide-up" default="none">
+                <ViewTransition enter="slide-up" update={{ 'tab-switch': 'auto', default: 'none' }} default="none">
                   <EventGrid searchParams={searchParams} />
                 </ViewTransition>
               </Suspense>

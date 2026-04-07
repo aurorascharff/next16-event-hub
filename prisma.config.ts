e11cd6bb66fb92de4b71e3a577383b6e17e8e322
Aurora Scharff <aurora.sofie@gmail.com>
2026-04-07 14:17:19 +0200

    Use local db

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,23 +2,16 @@ import dotenv from 'dotenv';
 import { defineConfig } from 'prisma/config';
 
 // --- PostgreSQL (Vercel) ---
-
-dotenv.config({ path: '.env.local' });
-
-export default defineConfig({
-  datasource: {
-    url: process.env.POSTGRES_PRISMA_URL!,
-  },
-  migrations: {
-    path: 'prisma/migrations',
-    seed: 'npx tsx prisma/seed.ts',
-  },
-  schema: 'prisma/schema.prisma',
-});
-
-// --- SQLite (local dev) ---
+// dotenv.config({ path: '.env.local' });
 // export default defineConfig({
-//   datasource: { url: 'file:./dev.db' },
+//   datasource: { url: process.env.POSTGRES_PRISMA_URL! },
 //   migrations: { path: 'prisma/migrations', seed: 'npx tsx prisma/seed.ts' },
 //   schema: 'prisma/schema.prisma',
 // });
+
+// --- SQLite (local dev) ---
+export default defineConfig({
+  datasource: { url: 'file:./dev.db' },
+  migrations: { path: 'prisma/migrations', seed: 'npx tsx prisma/seed.ts' },
+  schema: 'prisma/schema.prisma',
+});

08d4a11ae9d186ea7a4556ff80f692191d5532db
Aurora Scharff <aurora.sofie@gmail.com>
2026-04-07 15:58:41 +0200

    Add back real swl

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,16 +2,23 @@ import dotenv from 'dotenv';
 import { defineConfig } from 'prisma/config';
 
 // --- PostgreSQL (Vercel) ---
-// dotenv.config({ path: '.env.local' });
+
+dotenv.config({ path: '.env.local' });
+
+export default defineConfig({
+  datasource: {
+    url: process.env.POSTGRES_PRISMA_URL!,
+  },
+  migrations: {
+    path: 'prisma/migrations',
+    seed: 'npx tsx prisma/seed.ts',
+  },
+  schema: 'prisma/schema.prisma',
+});
+
+// --- SQLite (local dev) ---
 // export default defineConfig({
-//   datasource: { url: process.env.POSTGRES_PRISMA_URL! },
+//   datasource: { url: 'file:./dev.db' },
 //   migrations: { path: 'prisma/migrations', seed: 'npx tsx prisma/seed.ts' },
 //   schema: 'prisma/schema.prisma',
 // });
-
-// --- SQLite (local dev) ---
-export default defineConfig({
-  datasource: { url: 'file:./dev.db' },
-  migrations: { path: 'prisma/migrations', seed: 'npx tsx prisma/seed.ts' },
-  schema: 'prisma/schema.prisma',
-});

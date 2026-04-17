5be93d2eaeac6562be1c0645e2bd122152956894
Aurora Scharff <aurora.sofie@gmail.com>
2026-04-17 15:54:22 +0200

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  // // Simulate serverless environment like Vercel
+  // cacheMaxMemorySize: 0,
   experimental: {
     cachedNavigations: true,
+    instantNavigationDevToolsToggle: true,
+    optimisticRouting: true,
     prefetchInlining: true,
     useOffline: true,
     varyParams: true,

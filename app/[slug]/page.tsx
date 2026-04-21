f1d92da892ec2acffec7e988cbb9d1d46f8de15f
Aurora Scharff <aurora.sofie@gmail.com>
2026-04-21 09:47:30 -0400

    Update

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -7,7 +7,7 @@ import { getCommentsByEvent } from '@/data/queries/comment';
 import { getEventBySlug, getEvents } from '@/data/queries/event';
 import { CommentCard } from './_components/CommentCard';
 import { CommentForm } from './_components/CommentForm';
-import { EventDetails } from './_components/EventDetails';
+import { EventDetails, EventDetailsSkeleton } from './_components/EventDetails';
 import type { Metadata } from 'next';
 
 export async function generateMetadata({ params }: PageProps<'/[slug]'>): Promise<Metadata> {
@@ -30,11 +30,11 @@ export default async function SessionPage({ params }: PageProps<'/[slug]'>) {
   const { slug } = await params;
   return (
     <div className="flex flex-col gap-8">
-      <div className="min-h-72 sm:min-h-96">
-        <Suspense fallback={<CenteredSpinner />}>
+      <Suspense fallback={<EventDetailsSkeleton />}>
+        <div className="min-h-72 sm:min-h-96">
           <EventDetails slug={slug} />
-        </Suspense>
-      </div>
+        </div>
+      </Suspense>
       <div className="border-border/60 border-t pt-8">
         <div className="mb-6 min-h-9">
           <CommentForm />
